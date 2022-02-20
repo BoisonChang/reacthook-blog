6809f94eb575b804db2ce37ad842463806703850
BoisonChang <dragoncres@gmail.com>
2022-02-21 01:06:28 +0800

    avoid rerender by memo useMemo useCallback

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import TodoItem from './todoItem.js';
 import useTodos from './useTodos';
+import {memo, useMemo} from 'react';
+
+function Button({onClick, children}) {
+  return <button onClick={onClick}>{children}</button>
+}
+
+const MemoButton = memo(Button);
+
+
+const redStyle = {
+  borderColor: 'red'
+}
+const blueStyle = {
+  borderColor: 'blue'
+}
 
 function App() { 
   const {
@@ -10,11 +25,16 @@ function App() {
     value, 
     handleChange
   } = useTodos();
-  
+
+  const tryMemo = useMemo(() => {
+    console.log('calculte tryMemo color');
+    return value ? redStyle : blueStyle;
+  }, [value]) 
+
   return (
     <div className="App">
-      <input  type="text" placeholder="todo" value={value} onChange={handleChange} />
-      <button onClick={handleButtonClick}>Add Todo</button>
+      <input style={tryMemo}  type="text" placeholder="todo" value={value} onChange={handleChange} />
+      <MemoButton onClick={handleButtonClick}>Add Todo </MemoButton>
       {
         todos.map(todo => <TodoItem key={todo.id} todo={todo} handleToggledIsDone={handleToggledIsDone} handleDeleteTodo={handleDeleteTodo}></TodoItem>)
       }
