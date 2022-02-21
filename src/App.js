90cb005a0da75367d142409d64beb24fffcfccce
BoisonChang <dragoncres@gmail.com>
2022-02-21 15:15:02 +0800

    add decoration by styled components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import TodoItem from './todoItem.js';
 import useTodos from './useTodos';
-import React, {memo, useMemo}  from 'react';
+import React from 'react';
+import styled from 'styled-components';
 
-// function Button({onClick, children}) {
-//   return <button onClick={onClick}>{children}</button>
-// }
 
-class Button extends React.Component {
-  render() {
-    const {onClick, children} = this.props;
-    return <button onClick={onClick}>{children}</button>
+const Main = styled.div`
+  margin:4rem auto;
+  padding:2rem 3rem 3rem;
+  max-width:500px;
+  background:#FF6666;
+  color:#FFF;
+  box-shadow:-20px -20px 0px 0px rgba(100,100,100,.1);
+`
+
+const H1 = styled.div`
+  font-weight:normal;
+  font-size:2.6rem;
+  letter-spacing:0.05em;
+  border-bottom:1px solid rgba(255,255,255,.3); 
+  margin-bottom: 3rem;
+  span {
+    display:block;
+    font-size:0.8rem;
+    margin-bottom:0.7rem;
+    margin-left:3px;
+    margin-top:0.2rem;  
   }
-}
+`
 
+const Form = styled.form`
+  margin-top:3rem;
+  display:flex;
+  flex-wrap:wrap;
+`
 
+const Label = styled.label`
+  min-width:100%;
+  margin-bottom:.5rem;
+  font-size:1.3rem;
+`
 
-const MemoButton = memo(Button);
+const Input = styled.input`
+  flex-grow:1;
+  border:none;
+  background:#f7f1f1;
+  padding:0 1.5em;
+  font-size:initial;
+  font-family:'Quicksand', sans-serif;
+	height:3rem;
+`
 
-const redStyle = {
-  borderColor: 'red'
-}
-const blueStyle = {
-  borderColor: 'blue'
-}
+const Button = styled.button`
+  padding:0 1.3rem;
+  border:none;
+  background:#FF6666;
+  color:white;
+  text-transform:uppercase;
+  font-weight:bold;
+  border:1px solid rgba(255,255,255,.3);
+  margin-left:5px;
+  cursor:pointer;
+  transition:background .2s ease-out;
+`
 
 function App() { 
   const {
@@ -34,21 +73,25 @@ function App() {
     handleChange
   } = useTodos();
 
-  const tryMemo = useMemo(() => {
-    console.log('calculte tryMemo color');
-    return value ? redStyle : blueStyle;
-  }, [value]) 
+
 
   // 當輸入值改變才會重新計算，若是改變狀態就不會。
 
   return (
-    <div className="App">
-      <input style={tryMemo}  type="text" placeholder="todo" value={value} onChange={handleChange} />
-      <MemoButton onClick={handleButtonClick}>Add Todo </MemoButton>
+    <Main className="App">
+      <H1>
+        Todo List
+        <span>Get things done, one item at a time.</span>
+      </H1>     
       {
         todos.map(todo => <TodoItem key={todo.id} todo={todo} handleToggledIsDone={handleToggledIsDone} handleDeleteTodo={handleDeleteTodo}></TodoItem>)
       }
-    </div>
+      <Form>
+        <Label for="newitem">Add to the todo list</Label>
+        <Input  type="text" placeholder="todo" value={value} onChange={handleChange} />
+        <Button onClick={handleButtonClick}>Add Todo </Button>
+      </Form>
+    </Main>
   );
 }
 
--- src/todoItem.js
+++ src/todoItem.js
@@ -3,51 +3,55 @@ import { MEDIA_QUERY_MD, MEDIA_QUERY_LG } from './constants/style'
 import React from 'react';
 
 const TodoItemWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 8px 16px;
-  border: 1px solid black;
-  margin-top: 20px; 
+    max-width:500px;
+    display:flex;
+	margin:0 -3rem 4px;
+	padding:1.1rem 3rem;
+	justify-content:space-between;
+	align-items:center;
+	background:rgba(255,255,255,0.1);
 `
 
-const TodoContent = styled.div`
-  color: red;
-  ${props => props.$size === 'XL' && `font-size: 14px`}
-  ${({$isDone}) => $isDone && `
-    text-decoration: line-through;
-    color: grey;
-  ` }
+const TodoContent = styled.div`  
+    color: white;
+    font-size: 24px;
+    ${props => props.$size === 'XL' && `font-size: 28px`}
+    ${({$isDone}) => $isDone && `
+        text-decoration: line-through;
+        color: #afadad;
+    ` }
 `
-
   // 寫法 1： font-size: ${({size}) => size === 'XL' ? '20px' : '2px' }; 
   // 寫法 2：
 
 const TodoButtonWrapper = styled.div``
 const Button = styled.button`
-  padding: 4px;
-  color: ${ ({theme}) => theme.colors.primary_500 };
-  font-size: 30px;
+    color: white;
+    background-color: rgba(255,255,255,0.1);
+    padding: 10px 12px;
+    border:1px solid white;
+    ${({$isDone}) => $isDone && `
+        color: #af0505;
+    ` }
+    
 
-  &:hover {
-    color: red;
-  }
+    &:hover {
+        color: ${ ({theme}) => theme.colors.primary_500 };
+    }
 
-  & + & {
-    margin-left: 10px;
-  }
+    & + & {
+        margin-left: 10px;
+    }
 
-  ${MEDIA_QUERY_MD}{ 
-    font-size: 20px;
-  }
+    ${MEDIA_QUERY_MD}{ 
+        font-size: 14px;
+    }
 
-  ${MEDIA_QUERY_LG}{ 
-    font-size: 10px;
-  }
+    ${MEDIA_QUERY_LG}{ 
+        font-size: 16px;
+    }
 
 `
-
-
 export default class TodoItemC extends React.Component{
     constructor(props){
         super(props)
@@ -75,14 +79,14 @@ export default class TodoItemC extends React.Component{
 
         return (
             <TodoItemWrapper data-todo-id={todo.id}>
-            <TodoContent $isDone={todo.isDone} $size={size}>{todo.content}</TodoContent>
-            <TodoButtonWrapper>
-                <Button onClick={this.handleToggleClick}>
-                  {todo.isDone ?  '改成未完成' : '改成已完成' }
-              </Button>
-              <Button onClick={this.handleDeleteClick}>刪除</Button>
-            </TodoButtonWrapper>
-          </TodoItemWrapper>          
+                <TodoContent $isDone={todo.isDone} $size={size}>{todo.content}</TodoContent>
+                <TodoButtonWrapper>
+                    <Button $isDone={todo.isDone} onClick={this.handleToggleClick}>
+                        {todo.isDone ?  '改成未完成' : '改成已完成' }
+                    </Button>
+                <Button onClick={this.handleDeleteClick}>刪除</Button>
+                </TodoButtonWrapper>
+             </TodoItemWrapper>          
         )
     }
 }
